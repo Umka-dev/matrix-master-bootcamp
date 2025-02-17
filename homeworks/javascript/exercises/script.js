f0018e064d1da9f2d1e2b2894b3cc84b3c563ac3
Umka-dev <design.tvin@gmail.com>
2025-02-17 16:14:20 +0100

    feat: add script for exerscise 1

---
+++ homeworks/javascript/exercises/script.js
@@ -0,0 +1,16 @@
+// Part 1
+var age = 15;
+if (age >= 18) {
+  console.log('You can vote.');
+} else {
+  console.log('You cannot vote.');
+}
+
+// Part 2
+const userName = prompt('Enter your name:');
+
+if (userName === 'John') {
+  console.log('Hello, John!');
+} else {
+  console.log('You are not John.');
+}
