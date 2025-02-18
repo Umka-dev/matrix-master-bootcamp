1bae9a52629501dceb07cd3ff08a7d06147a4973
Umka-dev <design.tvin@gmail.com>
2025-02-18 21:18:13 +0100

    add 4th exercise

--- homeworks/javascript/exercises/7-dom-manipulation-exercises/script.js
+++ homeworks/javascript/exercises/7-dom-manipulation-exercises/script.js
@@ -21,3 +21,17 @@ function changeSource() {
 }
 
 // Exercise-4
+function checkValidation() {
+  let correctName = 'Mark';
+  let correctPassword = 'm6712k';
+
+  let userName = document.getElementById('userName').value;
+  console.log(userName);
+
+  let password = document.getElementById('userPassword').value;
+  console.log(password);
+
+  if (userName === correctName && password === correctPassword) {
+    alert('Form is submitted!');
+  } else alert('Wrong user name or password!');
+}
