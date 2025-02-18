e8e9a4bc2652c41010686f8f041aeeb05bae97cd
Umka-dev <design.tvin@gmail.com>
2025-02-18 20:59:42 +0100

    add 1, 2, 3 exercises of 4

--- homeworks/javascript/exercises/7-dom-manipulation-exercises/script.js
+++ homeworks/javascript/exercises/7-dom-manipulation-exercises/script.js
@@ -4,3 +4,20 @@ function changeText() {
 }
 
 // Exercise-2
+function addItem() {
+  const newListItem = document.createElement('li');
+  newListItem.textContent = 'new task';
+  document.querySelector('ul').appendChild(newListItem);
+}
+
+// Exercise-3
+function changeSource() {
+  document
+    .querySelector('img')
+    .setAttribute(
+      'src',
+      'https://www.piclumen.com/wp-content/uploads/2024/10/piclumen-upscale-after.webp'
+    );
+}
+
+// Exercise-4
