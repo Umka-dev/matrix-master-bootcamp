a59ce8043020bfc21ed296ceba1b15bf9998bdc7
Umka-dev <design.tvin@gmail.com>
2025-02-18 16:26:44 +0100

    add 1,2 exercises

--- homeworks/javascript/exercises/5-objects-as-classes-exercises/script.js
+++ homeworks/javascript/exercises/5-objects-as-classes-exercises/script.js
@@ -1,2 +1,20 @@
 //Exercise-1
-console.log();
+const Car = {
+  make: 'Toyota',
+  model: 'Camry',
+  year: 2022,
+};
+
+Car.start = function () {
+  console.log('Starting the car.');
+};
+
+console.log(Car);
+
+//Exercise-2
+Car.drive = function () {
+  console.log('Driving the car.');
+};
+
+Car.start();
+Car.drive();
