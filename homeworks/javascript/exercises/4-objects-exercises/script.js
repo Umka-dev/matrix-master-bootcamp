76cf9b41964a7124db6410862bcfd2d91304c282
Umka-dev <design.tvin@gmail.com>
2025-02-18 15:57:05 +0100

    add 1,2 exercises

--- homeworks/javascript/exercises/4-objects-exercises/script.js
+++ homeworks/javascript/exercises/4-objects-exercises/script.js
@@ -1,1 +1,23 @@
 // Exercise-1
+const book = {
+  title: 'Nineteen Eighty-Four',
+  author: 'George Orwell',
+  year: 1949,
+};
+
+console.log(book.title);
+console.log(book.author);
+console.log(book.year);
+
+// Exercise-2
+const person = {
+  name: 'Mike',
+  age: 33,
+  gender: 'male',
+};
+
+function printInfo(person) {
+  console.log(`Person: ${person.name}, ${person.age}, ${person.gender}`);
+}
+
+printInfo(person);
