8113eb207506cb0f6d26fe667fbced0d855589f1
Umka-dev <design.tvin@gmail.com>
2025-02-18 16:49:16 +0100

    add 1, 2 exercises

--- homeworks/javascript/exercises/6-window-object-exercises/script.js
+++ homeworks/javascript/exercises/6-window-object-exercises/script.js
@@ -1,1 +1,12 @@
 // Exercise-1
+function showAlert() {
+  window.alert('You clicked the button!');
+}
+
+// Exercise-2
+function getUserName() {
+  let userName = window.prompt('What is your name?');
+  window.alert(`Hello, ${userName}! Nice to meet you.`);
+}
+
+getUserName();
