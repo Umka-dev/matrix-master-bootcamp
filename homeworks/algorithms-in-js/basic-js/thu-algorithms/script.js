00180d55fabcc41b1925af96b6287ca0cdd0ebd3
Umka-dev <design.tvin@gmail.com>
2025-02-24 19:23:20 +0100

    add solution

--- homeworks/algorithms-in-js/basic-js/thu-algorithms/script.js
+++ homeworks/algorithms-in-js/basic-js/thu-algorithms/script.js
@@ -3,18 +3,25 @@
 // Return true if the given string is a palindrome. Otherwise, return false.
 // A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
-// Solution:
-// var checkPalindrome = function(word){
-// var inputWord = word;
-// var checkString = inputWord.split(" ").join("").toLowerCase();
-// for(var i = 0; i < checkString.length/2; i++){
-//     if(checkString[i] != checkString[checkString.length-i-1]){
-//       return false;
-//     }
-// }
-// return true;
-// }
-// var output = checkPalindrome("rasecar");
-// console.log(output);
-
 // My solution:
+// Create a function with a given argument str
+// Clear this string from spaces and make it lowercase
+// Reverse cleaned string
+// Compare clened string with reversed
+// Print the result
+
+const isPalindrome = (str) => {
+  let cleanedStr = str.split(' ').join('').toLowerCase(); // remove spaces and convert to lower case
+  let reverseStr = cleanedStr.split('').reverse().join(''); // split() converts the string to a character array, the reverse() method arranges the elements of the array in reverse order, convert the array back to a string
+  // console.log('Given string: ', str);
+  // console.log('Cleaned string: ', cleanedStr);
+  // console.log('Reversed string: ', reverseStr);
+  cleanedStr === reverseStr
+    ? console.log('Is Palindrome: ', true)
+    : console.log('Is Palindrome: ', false);
+};
+
+isPalindrome('maoam'); // true
+isPalindrome('Maoam'); // true
+isPalindrome('Maoams'); // false
+isPalindrome('Racecar'); // true
