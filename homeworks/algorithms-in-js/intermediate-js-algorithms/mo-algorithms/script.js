1c99b6446781648b7a8c5c40ff86065ff510a2a9
Umka-dev <design.tvin@gmail.com>
2025-02-24 22:35:30 +0100

    add the solution

--- homeworks/algorithms-in-js/intermediate-js-algorithms/mo-algorithms/script.js
+++ homeworks/algorithms-in-js/intermediate-js-algorithms/mo-algorithms/script.js
@@ -8,4 +8,40 @@
 // }
 // largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]); //Returns [5,27,39,1001]
 
-// My solution:
+// Solution:
+// Create a function that takes an array
+// Create a new empty array
+// For loop through the first level of the array
+// Add to the new array every first element from the subarrays
+// For loop through each subarray elements
+// If elements in the result array are smaller than elements in subarrays
+// Then replace them with the new largest values
+// Print  result array with largest numbers from subarrays
+
+const largestOfFour = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i][0]);
+    for (let j = 0; j < arr[i].length; j++) {
+      if (newArr[i] < arr[i][j]) {
+        newArr[i] = arr[i][j];
+      }
+    }
+  }
+  console.log(newArr);
+  return newArr;
+};
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]); // [5,27,39,1001]
+
+largestOfFour([
+  [4, 5, 100, 3],
+  [13, 270, 18, 26],
+  [32, 350, 37, 39],
+  [1000, 100, 857, 1],
+]); // [100, 270, 350, 1000]
