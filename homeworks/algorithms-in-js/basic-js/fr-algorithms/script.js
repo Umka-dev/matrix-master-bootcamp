e1c934b0a4f6bbf1a74dba78528301ad14ebc804
Umka-dev <design.tvin@gmail.com>
2025-02-24 21:20:51 +0100

    add solution

--- homeworks/algorithms-in-js/basic-js/fr-algorithms/script.js
+++ homeworks/algorithms-in-js/basic-js/fr-algorithms/script.js
@@ -6,6 +6,47 @@
 // //The correct answer is : 11
 
 // My solution:
+// Create a function that takes an array
+// Check an array length enough for the task
+// Create variables for the three highest numbers in the array
+// For loop through array elements
+// If the current element is higher than the first (which is 0) then we found the first highest number
+// Reissgn the values - third = second;  second = first;  first = num;
+// Else if the number is higher than the second and lower than the first
+// Then we found 2nd highest - second = num reassign the values
+// Else if the number is higher than the third and lower than the second
+// Then we found 3rd highest - third = num;
+// Print the 3rd highest
+// Test with other arrays
 
-//Using the array
-const arr = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
+const findThirdHighest = (arr) => {
+  if (arr.length < 3) {
+    console.log('Array must have at least 3 elements');
+    return;
+  }
+
+  let first = 0;
+  let second = 0;
+  let third = 0;
+
+  for (let num of arr) {
+    if (num > first) {
+      third = second;
+      second = first;
+      first = num;
+    } else if (num > second && num < first) {
+      third = second;
+      second = num;
+    } else if (num > third && num < second) {
+      third = num;
+    }
+  }
+
+  console.log('3rd highest:', third);
+  return third;
+};
+
+findThirdHighest([5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31]); // 11
+findThirdHighest([5, 2, 8, 20, -2, 0, 7, 3]); // 7
+findThirdHighest([5, 2, 8, -2, 0, 7]); // 5
+findThirdHighest([5, 2]); // Array must have at least 3 elements
