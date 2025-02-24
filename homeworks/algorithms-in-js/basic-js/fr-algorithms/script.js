28817f0c09c70df75533ff232a352c06a428c7ed
Umka-dev <design.tvin@gmail.com>
2025-02-24 19:33:22 +0100

    initial commit

---
+++ homeworks/algorithms-in-js/basic-js/fr-algorithms/script.js
@@ -0,0 +1,11 @@
+// Firday: 3rd Highest Number in Array
+// Having an array of numbers, write an algorithm that will return the 3rd highest number in the array. Your algorithm should do only one iteration of the array (just one for/while loop).
+// Example:
+// //Using the array
+// const arr = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31]
+// //The correct answer is : 11
+
+// My solution:
+
+//Using the array
+const arr = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
