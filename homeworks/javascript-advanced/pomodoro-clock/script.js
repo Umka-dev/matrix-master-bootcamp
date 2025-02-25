d6ed6a4ccacf56dc002eb9e27171ea56002c57b3
Umka-dev <design.tvin@gmail.com>
2025-02-25 09:56:54 +0100

    initial commit

---
+++ homeworks/javascript-advanced/pomodoro-clock/script.js
@@ -0,0 +1,9 @@
+// Assignment - Pomodoro Clock
+// Your assignment is to build a Pomodoro Clock, first of all, what is a Pomodoro Clock? Check this Wikipedia page for a nice description. (You may need to work in a team of two in order to make this assignment)
+
+// Points of interest:
+// Objective: Build an app that is functionally similar to this.
+// Fulfill the USER STORIES below. Use whichever libraries or APIs you need. Give it your own personal style.
+// User Story: I can start a 25-minute Pomodoro, and the timer will go off once 25 minutes have elapsed.
+// User Story: I can reset the clock for my next Pomodoro.
+// User Story: I can customize the length of each Pomodoro.
