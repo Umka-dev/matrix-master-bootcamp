af7b5295c8426eb839995885da696308aba98193
Umka-dev <design.tvin@gmail.com>
2025-03-05 12:13:55 +0100

    add solution

--- homeworks/algorithms-in-js/intermediate-js-algorithms/tu-algorithms/script.js
+++ homeworks/algorithms-in-js/intermediate-js-algorithms/tu-algorithms/script.js
@@ -5,3 +5,28 @@
 // findLongestWord("The quick brown fox jumped over the lazy dog")  // should return a number.
 // findLongestWord("The quick brown fox jumped over the lazy dog"). // should return 6.
 // findLongestWord("May the force be with you")  // should return 5.
+
+// My solution
+// Create a function that takes a string as a parameter
+// Define an array variable holding the original string as a string array
+// Difine an array variable holding every word length
+// For loop through the string array
+// If condition to check every word length is the longest
+// Then assign this word length to the variable 'the longest'
+// Return the largest word length as a number
+
+const findLongestWordLength = (str) => {
+  let arr = str.split(' ');
+  console.log(arr);
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i].length) {
+      max = arr[i].length;
+    }
+  }
+  console.log('Length of the longest word = ', max);
+  return max;
+};
+
+findLongestWordLength('Hello my friend !'); // 6
+findLongestWordLength('How are you doing today ?'); // 5
