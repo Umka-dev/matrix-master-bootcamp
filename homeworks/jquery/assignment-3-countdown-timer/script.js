bdf21f3fe8970dfc509d883c729d61ff87fb534a
Umka-dev <design.tvin@gmail.com>
2025-03-07 18:48:59 +0100

    initial commit

---
+++ homeworks/jquery/assignment-3-countdown-timer/script.js
@@ -0,0 +1,8 @@
+// Assignment 3: Countdown Timer
+// Your assignment is to build a TIMER using jQuery as in the pictures below
+
+// What you have to do:
+//  I can start a 25-minute, and the timer will go off once 25 minutes have elapsed.
+//  I can reset the clock for my next.
+//  I can customize the length of each time.
+//  I can get a sound notification when the time is finished.
