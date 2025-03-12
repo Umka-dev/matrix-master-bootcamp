ecd2e5dccc83138ff58a073ab2d199a81170b1a6
Umka-dev <design.tvin@gmail.com>
2025-03-12 12:43:12 +0100

    add solution

--- homeworks/algorithms-in-js/professional-js-algorithms/fr-algorithm-sum-all-odd-fibonacci/script.js
+++ homeworks/algorithms-in-js/professional-js-algorithms/fr-algorithm-sum-all-odd-fibonacci/script.js
@@ -9,3 +9,49 @@
 //   return num;
 // }
 // sumFibs(4);
+
+// Solution
+// Define a function that takes a number as a parameter
+// Define an empty array variable to hold the Fibonacci sequence
+// Define two variables to hold two first elements
+// Add these elements to the array
+// For loop to iterate through the array from i=2 to the given number
+// Define a variable to hold the next element as a sum of two previous numbers
+// Set prev number equal current number
+// Set curr number equal next
+// Add current number to the array
+// Return the array
+// Define the next function that takes fib array as a parameter
+// Define a variable to hold the sum
+// For loop to iterate through the array from i=0 to the length
+// Condition to check if the current fib number is odd
+// True - add it to the sum
+// Return the sum
+// Call the function
+
+const fibonacciSecuence = (num) => {
+  let fibArr = [];
+  let prev = 1;
+  let curr = 1;
+  fibArr.push(prev, curr);
+  for (let i = 3; i <= num; i++) {
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+    fibArr.push(curr);
+  }
+  return fibArr;
+};
+
+const sumOddFibs = (fibs) => {
+  let sum = 0;
+  for (let i = 0; i < fibs.length; i++) {
+    if (fibs[i] % 2 !== 0) {
+      sum += fibs[i];
+    }
+  }
+  return sum;
+};
+console.log(sumOddFibs(fibonacciSecuence(4))); // 5
+console.log(sumOddFibs(fibonacciSecuence(7))); // 23
+console.log(sumOddFibs(fibonacciSecuence(43))); // 784198803
