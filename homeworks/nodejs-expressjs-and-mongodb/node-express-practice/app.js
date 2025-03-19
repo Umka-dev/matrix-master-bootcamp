9f5ecf20e9b8be81c1543a9edbe30a9238a54742
Umka-dev <design.tvin@gmail.com>
2025-03-19 15:57:30 +0100

    create ejs views, render them, and pass title to them

--- homeworks/nodejs-expressjs-and-mongodb/node-express-practice/app.js
+++ homeworks/nodejs-expressjs-and-mongodb/node-express-practice/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { title } = require('process');
 
 // express app
 const app = express();
 
 // register view engine
 app.set('view engine', 'ejs');
+// app.set('views', 'my-views'); // in order to use other configuration with the different folder name for 'views'
 
 // listen for request
 const port = 3000;
@@ -12,21 +14,20 @@ app.listen(port);
 
 app.get('/', (req, res) => {
   //   res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
   //   res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a New Blog' });
 });
 
 // 404 page
 app.use((req, res) => {
   // use method means - if no one previous paths have matched then use the 404 page by default (that is similar to catch block)
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
