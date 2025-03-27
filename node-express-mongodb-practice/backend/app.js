a4d48c8dc310e3b5e6a5fa9cba8a6c24df415538
Umka-dev <design.tvin@gmail.com>
2025-03-27 17:02:21 +0100

    separate app to server, routes, controller modules (MVC)

--- node-express-mongodb-practice/backend/app.js
+++ node-express-mongodb-practice/backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 3300;
@@ -6,30 +7,7 @@ const PORT = 3300;
 // to our app can understand json
 app.use(express.json());
 
-// routes
-// home page
-
-app.get('/', (req, res) => {
-  console.log(req.params);
-  res.send(`<h1>Home page</h1>`);
-});
-
-// Get and display the request params on the page as a variable
-app.get('/:parameter', (req, res) => {
-  console.log(req.params);
-  res.send(`<h1>Hello ${req.params.parameter}</h1>`);
-});
-
-// contact page
-let users = [];
-app.get('/users', (req, res) => {
-  res.send(users);
-});
-
-// add new user
-app.post('/add-new-user', (req, res) => {
-  users.push(req.body);
-  res.send('User has been added');
-});
+// use routes
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
