53b84f1929ace2da7f227400cd0f71da8a9ab158
Umka-dev <design.tvin@gmail.com>
2025-03-27 15:58:21 +0100

    add get and post request and test them using Thunder Client, add use express json

--- node-express-mongodb-practice/backend/app.js
+++ node-express-mongodb-practice/backend/app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 
 const app = express();
-
 const PORT = 3300;
+
+// to our app can understand json
+app.use(express.json());
+
+// routes
+// home page
+app.get('/', (req, res) => {
+  res.send('I am in the home page');
+});
+
+// contact page
+let users = [];
+app.get('/contact', (req, res) => {
+  res.send(users);
+});
+
+// add new user
+app.post('/add-new-user', (req, res) => {
+  users.push(req.body);
+  res.send('User has been added');
+});
+
 app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
