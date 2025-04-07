d10e8e1bb99532ee9e83b6877774e6e85f0d584e
Umka-dev <design.tvin@gmail.com>
2025-04-07 16:41:35 +0200

    add two functional components

---
+++ homeworks/reactjs/first-react-app/react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from './components/Button';
+import { List } from './components/List';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello REACT!</h1>
+      <Button />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ homeworks/reactjs/first-react-app/react-app/src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from './Button';
+
+export const List = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Button />
+        </li>
+        <li>
+          <Button />
+        </li>
+      </ul>
+    </div>
+  );
+};
