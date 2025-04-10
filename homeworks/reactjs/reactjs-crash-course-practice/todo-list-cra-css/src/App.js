1b715f2fc605c0d1a934eadb68cd222670e0fcd8
Umka-dev <155664377+Umka-dev@users.noreply.github.com>
2025-04-10 14:34:39 +0200

    add todos list and todo item cpmponents, display list of items

--- homeworks/reactjs/reactjs-crash-course-practice/todo-list-cra-css/src/App.js
+++ homeworks/reactjs/reactjs-crash-course-practice/todo-list-cra-css/src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import React, { Component } from "react";
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
