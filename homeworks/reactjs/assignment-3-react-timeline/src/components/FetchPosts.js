d48819b4361d734a7e39d70017bb1195614a1ae9
Umka-dev <design.tvin@gmail.com>
2025-04-16 17:08:52 +0200

    change fetching from local API instead of outsourced one

--- homeworks/reactjs/assignment-3-react-timeline/src/components/FetchPosts.js
+++ homeworks/reactjs/assignment-3-react-timeline/src/components/FetchPosts.js
@@ -7,29 +7,43 @@ const FetchPosts = ({ onFetched }) => {
   // Fetch users (authors) from API
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then((res) => setUsers(res.data))
+      .get('http://localhost:3300/api/users')
+      .then((res) => setUsers(res.data.users))
       .catch((err) => console.error('Error fetching users:', err));
   }, []);
 
+  console.log('users:', users);
+
   // Fetch posts from API
   useEffect(() => {
-    if (users.length === 0) return; // Ensure users are loaded first
+    if (users.length === 0) return;
 
     axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+      .get('http://localhost:3300/api/posts')
       .then((res) => {
-        // Take the first post for each user
-        const postsFromUsers = users.map((user) => {
-          const post = res.data.find((post) => post.userId === user.id);
+        const allPosts = res.data.posts;
+        console.log('Posts: ', allPosts);
+
+        const postsWithUsers = allPosts.map((post) => {
+          const author = users.find(
+            (user) => user._id === post.user.toString()
+          );
+
           return {
-            userName: user.name,
-            message: post ? post.body : 'No post available',
-            date: new Date().toLocaleDateString(),
+            userName: author
+              ? `${author.first_name} ${author.last_name}`
+              : 'Unknown User',
+            message: post.message,
+            date: new Date(post.createdAt).toLocaleDateString(),
           };
         });
 
-        onFetched(postsFromUsers);
+        // Sort posts by date from newest to oldest
+        const sortedPosts = postsWithUsers.sort(
+          (a, b) => new Date(b.date) - new Date(a.date)
+        );
+
+        onFetched(sortedPosts);
       })
       .catch((err) => console.error('Error fetching posts:', err));
   }, [users, onFetched]);
