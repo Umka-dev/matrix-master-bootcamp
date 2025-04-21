6d8a578b3eedfef1b40d5b431e217924d32b7284
Umka-dev <design.tvin@gmail.com>
2025-04-21 12:57:14 +0200

    add pages and routes to app

--- homeworks/reactjs/demo-challenges/2-reddit-feed/src/App.js
+++ homeworks/reactjs/demo-challenges/2-reddit-feed/src/App.js
@@ -1,7 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+import Feed from './components/pages/Feed';
+import LoginPage from './components/pages/LoginPage';
+import NotFound from './components/pages/NotFound';
+
 import './App.css';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Navigate to='/feed' replace />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<LoginPage />} />
+
+          <Route path='/*' element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ homeworks/reactjs/demo-challenges/2-reddit-feed/src/components/pages/Feed.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <div>
+      <h1>Welcome, Damin Signh</h1>
+    </div>
+  );
+};
+export default Feed;
